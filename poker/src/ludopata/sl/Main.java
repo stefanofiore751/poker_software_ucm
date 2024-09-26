65df1078177a71a3919340f0d1550d744ad0b12b
laurareyes27 <laurreye@ucm.es>
2024-09-26 15:08:00 +0200

    apartados main

--- poker/src/ludopata/sl/Main.java
+++ poker/src/ludopata/sl/Main.java
@@ -4,10 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Game mainObj = new Game();
         // Apartado
-        mainObj.readInput();
-        mainObj.writeoutput();
+    	if(args.length < 6)
+        int apartado = Integer.parseInt(args[3]);
+        
+        switch(apartado) {
+        case 1:
+        	Game mainObj = new Game();
+        	mainObj.readInput();
+            mainObj.writeoutput();
+            break;
+        case 2:
+        	Game2 mainObj2 = new Game2();
+        	mainObj2.readInput();
+            mainObj2.writeoutput();
+            break;
+        }
+        
         //mainObj.measurePerformance(1000);
     }
 
